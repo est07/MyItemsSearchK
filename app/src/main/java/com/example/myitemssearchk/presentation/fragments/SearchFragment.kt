316ca205efa5d5d04f64247c4a68ccf199ebdc80
Esteban Serrano Vivas <est.2207@gmail.com>
2021-08-19 21:56:41 -0500

    Added designs and validation, to search fragment.

--- app/src/main/java/com/example/myitemssearchk/presentation/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/myitemssearchk/presentation/fragments/SearchFragment.kt
@@ -1,18 +1,45 @@
 package com.example.myitemssearchk.presentation.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.myitemssearchk.R
+import com.example.myitemssearchk.databinding.FragmentSearchBinding
 
-class SearchFragment : Fragment() {
+class SearchFragment : Fragment(R.layout.fragment_search) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSearchBinding.bind(view)
+
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.edtSearchField.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                validateSearchField()
+            }
+            false
+        }
+        binding.btnSearch.setOnClickListener {
+            validateSearchField()
+        }
+    }
+
+    private fun validateSearchField() {
+        if (!binding.edtSearchField.text?.trim().isNullOrEmpty()) {
+            gotoListItemsView(R.id.action_searchFragment_to_listItemsFragment)
+        } else {
+            binding.edtSearchField.error = getString(R.string.search_fragment_search_field_empty)
+        }
+    }
+
+    private fun gotoListItemsView(navigationAction: Int) {
+        view?.findNavController()?.navigate(navigationAction)
     }
 }
